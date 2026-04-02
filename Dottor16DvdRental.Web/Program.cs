cf8a20efc68157114ab223cc729f0133c6630943
RickyMandich <ricky.mandich@gmail.com>
2026-04-02 13:22:29 +0200

    aggiornamento 2026 04 02 13:22 - thursday launch break

--- Dottor16DvdRental.Web/Program.cs
+++ Dottor16DvdRental.Web/Program.cs
@@ -1,10 +1,25 @@
 using Dottor16DvdRental.Web.Components;
+using Dottor16DvdRental.Web.services.ActorsServices;
+using Dottor16DvdRental.Web.services.FilmsServices;
+using Dottor16DvdRental.Web.services.CategoriesServices;
+using Dottor16DvdRental.Web.services.JoinTables;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IActorsServices, ActorsServices>();
+builder.Services.AddScoped<IFilmsServices, FilmsServices>();
+builder.Services.AddScoped<ICategoriesServices, CategoriesServices>();
+
+builder.Services.AddScoped<FilmActor>();
+builder.Services.AddScoped<FilmCategory>();
+
+// traduce in automatico da snake_casing a PascalCasing nelle query al db
+// in modo che non serve fare snake_casing as PascalCasing
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var app = builder.Build();
 
