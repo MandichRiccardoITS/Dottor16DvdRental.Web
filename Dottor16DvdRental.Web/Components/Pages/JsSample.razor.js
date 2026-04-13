f51e9a82c4f6b6c05eee02fb5ac6164c20f9c951
RickyMandich <ricky.mandich@gmail.com>
2026-04-13 12:03:17 +0200

    aggiornamento 2026 04 13 12:03 - finish today lesson

---
+++ Dottor16DvdRental.Web/Components/Pages/JsSample.razor.js
@@ -0,0 +1,13 @@
+﻿export function showMessage(message) {
+    alert(message);
+}
+
+export function initTimer(dotNetCallback) {
+    setTimeout(function () {
+        dotNetCallback.invokeMethodAsync("SetText", "ciao da js")
+    }, 5000)
+}
+
+export function showActor(actor) {
+    alert(`Actor: ${actor.firstName} ${actor.lastName}`);
+}
---
+++ Dottor16DvdRental.Web/wwwroot/script.js
@@ -0,0 +1,13 @@
+﻿function add(a, b) {
+    return a + b;
+}
+
+function showText(text) {
+    alert(text);
+}
+
+function setContent(el, text) {
+    if (el) {
+        el.innerHTML = text;
+    }
+}
